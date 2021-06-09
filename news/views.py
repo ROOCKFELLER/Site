a538f9f1a5f486482b73759ed21127dcdf87b13e
ROOCKFELLER <roockfeller@gmail.com>
2021-06-09 10:14:31 +0300

    0906

--- news/views.py
+++ news/views.py
@@ -2,29 +2,41 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.views.generic import ListView
 from django.http import HttpResponse
 from .models import News, Category
-from .forms import NewsForm
+from .forms import NewsForm, UserRegisterForm, UserLoginForm
 from django.views.generic import ListView, DetailView, CreateView
 from django.urls import reverse_lazy
-from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
-
+from django.contrib.auth import login, logout
 
 def register(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = UserRegisterForm(request.POST)
         if form.is_valid():
-            form.save()
+            user = form.save()
+            login(request, user)
             messages.success(request, 'Успешно')
-            return redirect('login')
+            return redirect('add_news')
         else:
-            messages.error(request, 'Ошибка')
+            messages.error(request, 'Ошибка регистрации')
     else:
-        form = UserCreationForm()
+        form = UserRegisterForm()
     return render(request, 'news/register.html', {'form': form})
 
+def user_login(request):
+    if request.method == 'POST':
+        form = UserLoginForm(data = request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('add_news')
+    else:
+        form = UserLoginForm()
+    return render(request, 'news/login.html', {'form': form})
 
-def login(request):
-    return render(request, 'news/login.html')
+def user_logout(request):
+    logout(request)
+    return redirect('login')
+
 
 class HomeNews(ListView):
     model = News
